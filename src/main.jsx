3bc91133034f7088fbf216d46065438b468d04c0
melissaceee <cheng.melissa@outlook.com>
2024-09-10 18:26:57 -0400

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from './App.jsx'
 import './index.css'
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
